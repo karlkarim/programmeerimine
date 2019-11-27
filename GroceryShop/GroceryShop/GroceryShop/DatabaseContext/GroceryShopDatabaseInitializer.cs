d178783e84e9f9b0079abda71d8e1b7a871bf932
Opilane TTHK <opilane@tthk.ee>
2019-11-27 12:20:03 +0200

    tund27.11

---
+++ GroceryShop/GroceryShop/GroceryShop/DatabaseContext/GroceryShopDatabaseInitializer.cs
@@ -0,0 +1,46 @@
+﻿using GroceryShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace GroceryShop.DatabaseContext
+{
+    public class GroceryShopDatabaseInitializer : DropCreateDatabaseIfModelChanges<GroceryShopDbContext>
+    {
+        protected override void Seed(GroceryShopDbContext context)
+        {
+            context.Foods.AddOrUpdate(
+                x => x.Name,
+                new Food
+                {
+                  Name = "apple",
+                  Price = 1
+                },
+                new Food
+                {
+                    Name = "bread",
+                    Price = 0.6
+                },
+                new Food
+                {
+                    Name = "milk",
+                    Price = 0.9
+                },
+                new Food
+                {
+                    Name = "cake",
+                    Price = 2.1
+                },
+                new Food
+                {
+                    Name = "jam",
+                    Price = 1.1
+                }
+                );
+        }
+
+    }
+}
---
+++ GroceryShop/GroceryShop/GroceryShop/DatabaseContext/GroceryShopDbContext.cs
@@ -0,0 +1,23 @@
+﻿using GroceryShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace GroceryShop.DatabaseContext
+{
+    public class GroceryShopDbContext : DbContext
+    {
+        public DbSet<Food> Foods { get; set; }
+
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+        }
+    }
+}
